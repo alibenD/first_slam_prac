f051562a06a362299b972c4494f404cc2fd712b7
alibenD <aliben.develop@gmail.com>
2018-08-01 09:32:47 +0800

    Completed the camera class

--- src/camera.cc
+++ src/camera.cc
@@ -4,7 +4,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2018-07-31 20:49:07
-  * @last_modified_date: 2018-08-01 09:01:41
+  * @last_modified_date: 2018-08-01 09:32:25
   * @brief: Definition of Camera class
   */
 
@@ -16,37 +16,41 @@ namespace myslam
 {
     Eigen::Vector3d Camera::world2camera(const Eigen::Vector3d& point_world, const Sophus::SE3& T_camera_world)
     {
-      Eigen::Vector3d coor_point_camera;
+      Eigen::Vector3d coor_point_camera = T_camera_world*point_world;
       return coor_point_camera;
     }
 
     Eigen::Vector2d Camera::world2pixel(const Eigen::Vector3d& point_world, const Sophus::SE3& T_camera_world)
     {
-      Eigen::Vector2d coor_point_pixel;
+      Eigen::Vector2d coor_point_pixel = camera2pixel(point_world*T_camera_world);
       return coor_point_pixel;
     }
 
     Eigen::Vector3d Camera::camera2world(const Eigen::Vector3d& point_camera, const Sophus::SE3& T_world_camera)
     {
-      Eigen::Vector3d coor_point_world;
+      Eigen::Vector3d coor_point_world = T_world_camera.inverse()*point_camera;
       return coor_point_world;
     }
 
     Eigen::Vector2d Camera::camera2pixel(const Eigen::Vector3d& point_camera)
     {
-      Eigen::Vector2d coor_point_pixel;
+      float x_tmp = fx_*point_camera(0,0) / point_camera(2,0) + cx_;
+      float y_tmp = fy_*point_camera(1,0) / point_camera(2,0) + cy_;
+      Eigen::Vector2d coor_point_pixel(x_tmp, y_tmp);
       return coor_point_pixel;
     }
 
     Eigen::Vector3d Camera::pixel2camera(const Eigen::Vector2d& point_pixel, double depth=1)
     {
-      Eigen::Vector3d coor_point_camera;
+      float x_tmp = (point_pixel(0,0) - cx_) * depth / fx_;
+      float y_tmp = (point_pixel(1,0) - cy_) * depth / fy_;
+      Eigen::Vector3d coor_point_camera(x_tmp, y_tmp, depth);
       return coor_point_camera;
     }
 
     Eigen::Vector3d Camera::pixel2world(const Eigen::Vector2d& point_pixel, const Sophus::SE3& T_camera_world, double depth=1)
     {
-      Eigen::Vector3d coor_point_world;
+      Eigen::Vector3d coor_point_world camera2world( pixel2camera(point_pixel, depth), T_camera_world );
       return coor_point_world;
     }
 }   // END of namespace myslam
