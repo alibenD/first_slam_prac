f142d8894d2db2c7fd17125c9ad26801f00ce68c
alibenD <aliben.develop@gmail.com>
2018-08-03 09:14:05 +0800

    Added a header file for including all of .hh file in myslam

---
+++ include/myslam.hh
@@ -0,0 +1,25 @@
+#ifndef __MYSLAM_HH__
+#define __MYSLAM_HH__
+/**-----------------------------------------------
+  * @Copyright (C) 2018 All rights reserved.
+  * @date: 2018
+  * @file: myslam.hh
+  * @version: v0.0.1
+  * @author: aliben.develop@gmail.com
+  * @create_date: 2018-08-03 09:12:21
+  * @last_modified_date: 2018-08-03 09:13:34
+  * @brief: TODO
+  * @details: TODO
+  *-----------------------------------------------*/
+
+// Header include
+#include <myslam/common.hh>
+#include <myslam/config.hh>
+#include <myslam/camera.hh>
+#include <myslam/frame.hh>
+#include <myslam/map.hh>
+#include <myslam/mappoint.hh>
+#include <myslam/visual_odometry.hh>
+
+// Declaration
+#endif // __MYSLAM_HH__
