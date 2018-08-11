4f5e8ad9336198e36c24f8fbecd020a917225caa
alibenD <aliben.develop@gmail.com>
2018-08-12 01:19:47 +0800

    Added a test example for Frame

---
+++ test/test_frame.cc
@@ -0,0 +1,21 @@
+/**
+  * @Copyright (C) 2018 All rights reserved.
+  * @date: 2018
+  * @file: test_frame.cc
+  * @version: v0.0.1
+  * @author: aliben.develop@gmail.com
+  * @create_date: 2018-08-12 00:28:49
+  * @last_modified_date: 2018-08-12 00:31:27
+  * @brief: TODO
+  * @details: TODO
+  */
+
+//INCLUDE
+#include <myslam/frame.hh>
+
+//CODE
+int main(int argc, char** argv)
+{
+  myslam::Frame::Ptr pFrame = std::make_shared<myslam::Frame>();
+  return 0;
+}
