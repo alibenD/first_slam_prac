f82983772336b489b63d3e099a8fe7d3fa6707fe
alibenD <aliben.develop@gmail.com>
2018-08-13 15:36:57 +0800

    Try

--- src/visual_odometry.cc
+++ src/visual_odometry.cc
@@ -5,7 +5,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2018-08-02 10:35:36
-  * @last_modified_date: 2018-08-13 15:21:34
+  * @last_modified_date: 2018-08-13 15:36:32
   * @brief: TODO
   * @details: TODO
   */
@@ -203,7 +203,8 @@ namespace myslam
     Eigen::Quaterniond q(r);
     std::cout << "[Bebug] Before set_tcw_estimate" << std::endl;
     //T_curr_ref_estimated_ = Sophus::SE3<double>(Sophus::SO3<double>(q), Eigen::Vector3d(tvec.at<double>(0,0), tvec.at<double>(1,0), tvec.at<double>(2,0)));
-    T_curr_ref_estimated_ = Sophus::SE3<double>(r, t);
+    //T_curr_ref_estimated_ = Sophus::SE3<double>(r, t);
+    Sophus::SE3<double>(r, t);
     std::cout << "[Bebug] After set_tcw_estimate" << std::endl;
     //std::cout << "Tcr: " << tcre << std::endl; 
     //setTransformationEstimation(tcre);
