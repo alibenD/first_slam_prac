31eb88d0a4963a9b2c06328cee8392796cf7a062
alibenD <aliben.develop@gmail.com>
2018-08-16 11:47:18 +0800

    Fixed shared_ptr new bug

--- src/visual_odometry.cc
+++ src/visual_odometry.cc
@@ -5,7 +5,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2018-08-02 10:35:36
-  * @last_modified_date: 2018-08-13 15:50:52
+  * @last_modified_date: 2018-08-16 11:46:19
   * @brief: TODO
   * @details: TODO
   */
@@ -200,13 +200,12 @@ namespace myslam
     cv::cv2eigen(R, r);
     cv::cv2eigen(tvec, t);
 
-    //Eigen::AngleAxisd angle(r);
+    Eigen::AngleAxisd angle(r);
     Eigen::Quaterniond q(r);
-    std::cout << "[Bebug] Before set_tcw_estimate" << std::endl;
+    //std::cout << "[Bebug] Before set_tcw_estimate" << std::endl;
     //T_curr_ref_estimated_ = Sophus::SE3<double>(Sophus::SO3<double>(q), Eigen::Vector3d(tvec.at<double>(0,0), tvec.at<double>(1,0), tvec.at<double>(2,0)));
-    //T_curr_ref_estimated_ = Sophus::SE3<double>(r, t);
-    Sophus::SE3<double>(r, t);
-    std::cout << "[Bebug] After set_tcw_estimate" << std::endl;
+    T_curr_ref_estimated_ = Sophus::SE3<double>(r, t);
+    //std::cout << "[Bebug] After set_tcw_estimate" << std::endl;
     //std::cout << "Tcr: " << tcre << std::endl; 
     //setTransformationEstimation(tcre);
     return 0;
