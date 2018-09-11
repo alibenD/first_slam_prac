e757b0f367340c25f55b7f488e5746186b9d6661
alibenD <aliben.develop@gmail.com>
2018-09-11 16:08:42 +0800

    Updated test_frame

--- examples/test_frame.cc
+++ examples/test_frame.cc
@@ -5,7 +5,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @create_date: 2018-09-10 15:32:58
-  * @last_modified_date: 2018-09-10 17:04:38
+  * @last_modified_date: 2018-09-10 21:31:47
   * @brief: TODO
   * @details: TODO
   */
@@ -19,6 +19,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 //CODE
 int main(int argc, char** argv)
@@ -34,6 +35,14 @@ int main(int argc, char** argv)
   std::string dataset_dir = p_camera->get<std::string>("dataset_dir", *p_camera);
   std::cout << "Dataset: " << dataset_dir << std::endl;
   std::ifstream fin(dataset_dir + "/associate.txt");
+  std::vector<myslam::Frame::Ptr> p_frames;
+
+  cv::Ptr<cv::ORB> p_orb = cv::ORB::create(400);
+
+  std::vector<cv::KeyPoint> keypoint_last, keypoint_current;
+  cv::Mat descriptor_last, descriptor_current;
+  cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
+
 
   cv::namedWindow("Show Image", cv::WINDOW_NORMAL);
 
@@ -61,8 +70,55 @@ int main(int argc, char** argv)
     cv::Mat image = cv::imread(image_filename);
 
     auto p_frame = myslam::Frame::createFrame(image, p_camera, timestamp);
+    std::cout << *p_frame << std::endl;
+    p_orb->detect(p_frame->get_color(), keypoint_current);
+    p_orb->compute(p_frame->get_color(), keypoint_current, descriptor_current);
 
-    cv::imshow("Show Image", p_frame->get_color());
+    p_frame->set_descriptor(descriptor_current);
+    p_frame->set_keypoint(keypoint_current);
+
+    p_frames.push_back(p_frame);
+    int size = p_frames.size();
+    if(size < 2)
+    {
+      continue;
+    }
+
+    auto descriptor_last = p_frames[size-2]->get_descriptor();
+    auto keypoint_last = p_frames[size-2]->get_keypoint();
+    auto image_last = p_frames[size-2]->get_color();
+
+    std::vector<cv::DMatch> matches;
+    matcher->match(descriptor_last, descriptor_current, matches);
+
+    // GOOD matches
+    double min_dist = 10000, max_dist = 0.;
+    std::vector<cv::DMatch> good_matches;
+    for(int i=0; i < matches.size(); i++)
+    {
+      double dist = matches[i].distance;
+      if(dist < min_dist)
+      {
+        min_dist = dist;
+      }
+      if(dist > max_dist)
+      {
+        max_dist = dist;
+      }
+    }
+
+    for(cv::DMatch& m : matches)
+    {
+      if(m.distance < std::max<float>(min_dist*1.5, 30.0))
+      {
+        good_matches.push_back(m);
+      }
+    }
+
+    cv::Mat matched_image;
+    cv::drawMatches(image_last, keypoint_last, image, keypoint_current, good_matches, matched_image);
+    cv::imshow("Show Image", matched_image);
+    //cv::imshow("Show Image", p_frame->get_color());
     cv::waitKey(30);
 
   }
